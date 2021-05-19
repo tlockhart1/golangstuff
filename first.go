e6bc6c0ae1c1c7d767c08daf435bbda6b15d67a3
tlockhart1 <tlockhart1@crimson.ua.edu>
2021-05-19 14:42:20 -0400

    first commit

---
+++ first.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://edmundmartin.com/writing-a-web-crawler-in-golang/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	robots, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s", robots)
+}
